220f396745ad700aef2bdbab49ae8206eda7185c
geethatulasi-nyros <geethatulasi.nyros@gmail.com>
2019-10-21 14:06:34 +0530

    message

--- lib/great.rb
+++ lib/great.rb
@@ -1,10 +1,20 @@
-require "great/version"
+# require "great/version"
 
 module Great
-  class Error < StandardError; end
-  class Text
-  	def self.hi
-  		puts "hello"
-  	end
-  end
+	class Text
+		def self.hi
+			puts "hello"
+		end
+		def initialize
+			puts "initialised"
+		end
+		def add(a,b)
+			res = a.to_i + b.to_i
+			res
+		end
+		def sub(a,b)
+			res = a.to_i - b.to_i
+			res
+		end
+	end
 end
