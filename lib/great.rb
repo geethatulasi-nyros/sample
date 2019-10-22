558c6232053089e79876b292192a53a6311f6cd2
geethatulasi-nyros <geethatulasi.nyros@gmail.com>
2019-10-22 12:13:03 +0530

    6 th version

--- lib/great.rb
+++ lib/great.rb
@@ -1,6 +1,12 @@
 # require "great/version"
 require 'active_support/inflector'
+require 'active_record'
+
 module Great
+	autoload :FullTextIndexer,         'great/full_text_indexer'
+  autoload :PostgresModuleInstaller, 'great/postgres_module_installer'
+  autoload :TrigramInstaller,        'great/trigram_installer'
+  autoload :MigrationGenerator,      'great/migration_generator'
 	class Text
 		def self.hi
 			puts "hello"
@@ -26,5 +32,20 @@ module Great
 		def self.pluralize(word)
 			word.pluralize
 		end
+		def self.searchable_language
+    	'english'
+  	end
+
+  private
+  def searchable_columns
+    columns.select {|column| [:string, :text].include? column.type }.map(&:name)
+  end
+  def quoted_language
+    @quoted_language ||= connection.quote(searchable_language)
+  end
+  def searchable_language
+    Great.searchable_language
+  end
+
 	end
 end
--- lib/great/version.rb
+++ lib/great/version.rb
@@ -1,3 +1,3 @@
 module Great
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 end
